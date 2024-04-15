763d5a82d82329062b96f3794e552bfcb45e9604
Carlos Quesada <cquesadad90@gmail.com>
2024-04-15 12:17:12 +0100

    check if custom price option is active

--- public/custom-price-display.php
+++ public/custom-price-display.php
@@ -8,14 +8,22 @@ function crbp_get_custom_price_by_role($product_id) {
     $roles = $user->roles;
     $custom_price = '';
 
-    // VErify each role and get custom price 
-    foreach ($roles as $role) {
-        $role_custom_price = get_post_meta($product_id, 'custom_price_' . $role, true);
-        if (!empty($role_custom_price)) {
-            // If exist custom price return it
-            return $role_custom_price;
-        }
-    }
+     // Verificar si se debe mostrar el precio personalizado
+     $show_custom_price = get_option('cpur_show_hide_prices', 1); // Valor predeterminado a 1 (activo)
+
+     // Si la opción de mostrar precios está desactivada, retornar precio vacío
+     if (!$show_custom_price) {
+         return $custom_price;
+     }
+ 
+     // Verificar cada rol y obtener el precio personalizado
+     foreach ($roles as $role) {
+         $role_custom_price = get_post_meta($product_id, 'custom_price_' . $role, true);
+         if (!empty($role_custom_price)) {
+             // Si existe un precio personalizado, devolverlo
+             return $role_custom_price;
+         }
+     }
 
     // If there is no custom price for rolle retun empty custom price '' 
     return $custom_price;
@@ -36,5 +44,20 @@ function crbp_display_custom_price($price, $product) {
     return $price;
 }
 add_filter('woocommerce_get_price_html', 'crbp_display_custom_price', 10, 2);
+
+// Show custom price in mini cart
+function crbp_elementor_pro_cart_item_price($price, $cart_item, $cart_item_key) {
+    // Get Product ID
+    $product_id = $cart_item['product_id'];
+    // Get custom price
+    $custom_price = crbp_get_custom_price_by_role($product_id);
+    // Verify if there is custom price
+    if (!empty($custom_price)) {
+        $price = wc_price($custom_price);
+    }
+    
+    return $price;
+}
+add_filter('woocommerce_cart_item_price', 'crbp_elementor_pro_cart_item_price', 10, 3);
 
 ?>
